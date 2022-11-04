8db4ec59a62a312579b2e943cea5e136e3c53f43
mattyshalom <olusegesunday@yahoo.com>
2022-11-04 12:55:29 +0100

    file_count

---
+++ python/file_counts.py
@@ -0,0 +1,8 @@
+# counting total number of files and their extention type
+file_counts = {"jpg":10, "txt":14, "csv":2, "py":23}
+for extension in file_counts:
+    print(extension)
+
+
+for ext, amount in file_counts.items():
+    print("There are {} files with the .{} extenson" .format(amount, ext))
