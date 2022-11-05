6f964efa55053998840e8285f90eeb5776ccc7e5
mattyshalom <olusegesunday@yahoo.com>
2022-11-05 11:55:42 +0100

    flower_color

---
+++ python/flower_color.py
@@ -0,0 +1,16 @@
+# a program that print the color of a flower
+
+class Flower:
+  color = 'unknown'
+
+rose = Flower()
+rose.color = "Red"
+
+violet = Flower()
+violet.color = "blue"
+
+this_pun_is_for_you = Flower()
+
+print("Roses are {},".format(rose.color))
+print("violets are {},".format(violet.color))
+print(this_pun_is_for_you) 
